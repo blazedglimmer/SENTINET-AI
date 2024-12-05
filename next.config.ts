b0ced97399c6e30fdc28edb11515d69fed40e72b
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-12-06 03:36:43 +0530

    Add changes to next config, and package json to fix the build at server

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   webpack: config => {
-    config.resolve.extensions = ['.ts', '.tsx', '.js', '.jsx'];
-    config.resolve.alias['@'] = __dirname;
+    config.resolve.alias['tailwindcss'] = require.resolve('tailwindcss');
     return config;
   },
 };
