298969bc5ba4bee8be3da768fc76584c412d435e
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-12-06 03:24:00 +0530

    Update to get away with server build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: config => {
+    config.resolve.extensions = ['.ts', '.tsx', '.js', '.jsx'];
+    config.resolve.alias['@'] = __dirname;
+    return config;
+  },
 };
 
 export default nextConfig;
